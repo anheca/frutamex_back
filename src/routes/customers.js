4321c9b2269f62d4532c178ab22bfcb12f2d21da
anheca <anheca68@gmail.com>
2020-04-16 19:21:28 -0300

    Se agrego el componente de clientes html, css etc

--- src/routes/customers.js
+++ src/routes/customers.js
@@ -3,4 +3,12 @@ const router = express.Router();
 
 const pool = require("../database");
 
+router.get("/add", (req, res) => {
+  res.render("customers/add");
+});
+
+router.post("/add", (req, res) => {
+  res.send("recibido");
+});
+
 module.exports = router;
